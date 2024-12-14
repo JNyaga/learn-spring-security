3ac6d4ae3df013ad3f553fc5d49f3b96de73b285
JNyaga <joelnyagamuriithi@gmail.com>
2024-12-14 18:38:10 +0300

    Exploring Spring Security - Storing User Credentials in memory

--- src/main/java/com/ropro/learn_spring_security/basic/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/ropro/learn_spring_security/basic/BasicAuthSecurityConfiguration.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -28,6 +31,21 @@ public class BasicAuthSecurityConfiguration {
         http.csrf(csrf -> csrf.disable());
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailService() {
+        var user = User.withUsername("joel")
+                .password("{noop}dummy")
+                .roles("USER")
+                .build();
+
+        var admin = User.withUsername("admin")
+                .password("{noob}admin")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
 }
