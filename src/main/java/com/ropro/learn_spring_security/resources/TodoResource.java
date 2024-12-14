6768e096eaf5828ec1e4f86d1e4635ccaad0761a
JNyaga <joelnyagamuriithi@gmail.com>
2024-12-14 17:37:00 +0300

    Refactor HelloWorldResource and add TodoResource with CRUD operations- Exploring Spring Security - Cross Site Request Forgery - CSRF

---
+++ src/main/java/com/ropro/learn_spring_security/resources/TodoResource.java
@@ -0,0 +1,39 @@
+package com.ropro.learn_spring_security.resources;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoResource {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    private static final List<Todo> TODO_LIST = List.of(
+            new Todo("John", "Learn Spring Boot"),
+            new Todo("Perry", "Growth Hacking"));
+
+    @GetMapping("/todos")
+    public List<Todo> retrieveAllTodos() {
+        return TODO_LIST;
+    }
+
+    @GetMapping("/users/{username}/todos")
+    public Todo retrieveTodosForSpecificUser(@PathVariable String username) {
+        return TODO_LIST.get(0);
+    }
+
+    @PatchMapping("/users/{username}/todos")
+    public void createTodoForSPecificUser(@PathVariable String username, @RequestBody Todo todo) {
+        logger.info("Creat {} for {}", todo, username);
+    }
+
+}
+
+record Todo(String username, String description) {
+}
