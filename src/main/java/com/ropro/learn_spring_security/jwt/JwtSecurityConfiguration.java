f5f09a0d0d57b307af3d165346abf3c5f469e9a1
JNyaga <joelnyagamuriithi@gmail.com>
2024-12-19 00:22:23 +0300

    Add JwtEncoder bean to JwtSecurityConfiguration for JWT encoding

--- src/main/java/com/ropro/learn_spring_security/jwt/JwtSecurityConfiguration.java
+++ src/main/java/com/ropro/learn_spring_security/jwt/JwtSecurityConfiguration.java
@@ -13,7 +13,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -130,6 +132,11 @@ public class JwtSecurityConfiguration {
                 .withPublicKey(rsaKey.toRSAPublicKey())
                 .build();
 
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder(JWKSource<SecurityContext> jwtSource) {
+        return new NimbusJwtEncoder(jwtSource);
     }
 
 }
