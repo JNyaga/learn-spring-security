12ca726b9d9f5750d6c222fbbc3ce87a2eeb2e45
JNyaga <joelnyagamuriithi@gmail.com>
2024-12-18 11:12:32 +0300

    Update pom.xml and BasicAuthSecurityConfiguration for H2 database integration ,  user management and hashing (pswd)

--- src/main/java/com/ropro/learn_spring_security/basic/BasicAuthSecurityConfiguration.java
+++ src/main/java/com/ropro/learn_spring_security/basic/BasicAuthSecurityConfiguration.java
@@ -4,14 +4,21 @@ import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class BasicAuthSecurityConfiguration {
@@ -30,22 +37,62 @@ public class BasicAuthSecurityConfiguration {
         // disable csrf
         http.csrf(csrf -> csrf.disable());
 
+        // enable frame options
+        http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()));
+
         return http.build();
     }
 
+    // @Bean
+    // public UserDetailsService userDetailService() {
+    // var user = User.withUsername("joel")
+    // .password("{noop}dummy")
+    // .roles("USER")
+    // .build();
+
+    // var admin = User.withUsername("admin")
+    // .password("{noob}admin")
+    // .roles("ADMIN")
+    // .build();
+
+    // return new InMemoryUserDetailsManager(user, admin);
+    // }
+
     @Bean
-    public UserDetailsService userDetailService() {
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailService(DataSource dataSource) {
         var user = User.withUsername("joel")
-                .password("{noop}dummy")
+                // .password("{noop}dummy")
+                .password("dummy")
+                .passwordEncoder(str -> passwordEncoder().encode(str))
                 .roles("USER")
                 .build();
 
         var admin = User.withUsername("admin")
-                .password("{noob}admin")
-                .roles("ADMIN")
+                // .password("{noob}admin")
+                .password("dummy")
+                .passwordEncoder(str -> passwordEncoder().encode(str))
+                .roles("ADMIN", "USER")
                 .build();
 
-        return new InMemoryUserDetailsManager(user, admin);
+        var jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        jdbcUserDetailsManager.createUser(user);
+        jdbcUserDetailsManager.createUser(admin);
+
+        return jdbcUserDetailsManager;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
